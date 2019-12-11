6262c6d30e0056e63a33df282986f580dc4dde83
Przemysław Matkowski <matkowski1994@gmail.com>
2019-12-11 15:40:30 +0100

    Updates

---
+++ precache-manifest.81057c6f2142420f7831ee5333120b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863e019c5f6d7c8281ca783f239a59d5",
+    "url": "/resurs/index.html"
+  },
+  {
+    "revision": "65c9ad226125c4fcdfd0",
+    "url": "/resurs/static/css/main.7f6395ea.chunk.css"
+  },
+  {
+    "revision": "a436d3b48785f1e64cfb",
+    "url": "/resurs/static/js/2.9118d9d1.chunk.js"
+  },
+  {
+    "revision": "e4a6a4fbe69b52905966f9a3b585598f",
+    "url": "/resurs/static/js/2.9118d9d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65c9ad226125c4fcdfd0",
+    "url": "/resurs/static/js/main.e583b0f7.chunk.js"
+  },
+  {
+    "revision": "373afaa666f68e05a9c7",
+    "url": "/resurs/static/js/runtime-main.16e97d79.js"
+  }
+]);
